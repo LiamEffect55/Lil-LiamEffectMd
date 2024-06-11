655e6a995cdbec73ca3f3db6f946f095d76f5360
Malvin King <163745886+malvinking@users.noreply.github.com>
2024-06-11 10:40:34 +0200

    Update hacker2.js

--- plugins/hacker2.js
+++ plugins/hacker2.js
@@ -24,15 +24,15 @@
 //                                                                                            //
 //════════════════════════════════════════════════════════════════════════════════════════════//
 *                                                                 
-  * @project_name : Venocyber-Md
-   * @author : Venocyber Tech
-   * @youtube : https://www.youtube.com/@JASTINMTEWA-vn9pl
-   * @description : Venocyber-Md ,A Multi-functional whatsapp user bot.
+  * @project_name : Speed-Md 
+   * @author : Malvin King 
+   * @youtube : https://www.youtube.com/@speedtechi
+   * @description : Speed-Md ,A Multi-functional whatsapp user bot.
    * @version 2.0.0
 *
 * 
-   * Created By Venocyber Tech.
-   * © 2024 Venocyber-Md.
+   * Created B Malvin King.
+   * © 2024 Speed-Md.
 
 
 */
